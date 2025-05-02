bcaf43106072edf108601cbf945fc6a5b66ba158
Pavithra <pavithrasai9398@gmail.com>
2025-05-02 21:18:55 +0400

    Update Dashboard.js

--- src/pages/Admin/Dashboard.js
+++ src/pages/Admin/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom'; // ✅ Step 1: Import Link
+import { Link } from 'react-router-dom';
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
